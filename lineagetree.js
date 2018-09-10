c2c6d8c911b9c34d6e086e70f18c86729b643cc3
hshore29 <h.shore29@gmail.com>
2018-09-10 08:13:12 -0400

    JS Refactor - better init / load data workflow

--- lineagetree.js
+++ lineagetree.js
@@ -1,5 +1,56 @@
+/*** Lineage Tree Drawing Class ***/
+function LineageTree(container, logoURL) {
+  let _ = this;
+  _.box = {width: 130, height: 56, margin: 20};
+  _.navHeight = 150;
+  _.logoHeight = 3/4;
+  _.logoOpacity = 0.25;
+  _.k = 1;
+  _.backgroundURL = logoURL || null;
+  
+  // Create main containers
+  _.body = d3.select(container);
+  _.svg = _.body.append("svg").attr("width", "100%").attr("height", "100%");
+  _.view = _.svg.append("g").attr("class", "view-box");
+  _.nav = _.body.append("div").attr("class", "nav container").append("svg");
+  
+  // Set up background containers
+  if (_.backgroundURL) {
+    _.bkgSource = _.body.append("div").style("display", "none")
+        .append("svg").attr("id", "background");
+    _.viewUse = _.view.append("use").attr("xlink:href", "#background")
+        .attr("opacity", _.logoOpacity);
+    _.navUse = _.nav.append("use").attr("xlink:href", "#background")
+        .attr("opacity", _.logoOpacity);
+  }
+
+  // Add graph layers
+  _.view.append("g").attr("class", "link-layer");
+  _.view.append("g").attr("class", "node-layer");
+  _.nav.append("g").attr("class", "link-layer");
+  _.nav.append("g").attr("class", "node-layer");
+
+  // Tree functions
+  _.stratify = d3.stratify().id(d => d.name).parentId(d => d.parent);
+  _.tree = d3.tree()
+      .nodeSize([_.box.width + _.box.margin, _.box.height + _.box.margin])
+      .separation((a, b) => {
+        // If we share a parent, keep padding tight
+        if (a.parent == b.parent) return 1;
+        // Otherwise, add padding so we don't overlap either node's children
+        let sep = 1;
+        if (a.children) sep += (a.children.length - 1) / 2;
+        if (b.children) sep += (b.children.length - 1) / 2;
+        return sep;
+      })
+
+  // Define nav box scales
+  _.navScaleY = d3.scaleLinear();
+  _.navScaleX = d3.scaleLinear();
+}
+
 /*** Google Sheets Call Back ***/
-function drawGraph(data) {
+LineageTree.prototype.parseGoogleSheet = function(data) {
   let rawdata = [];
   // Convert Google Sheets JSON to list of lists
   data.feed.entry.forEach(function(cell) {
@@ -17,53 +68,33 @@ function drawGraph(data) {
   let cleandata = [];
   let header = rawdata.shift();
   rawdata.forEach(function(row, index) {
-    cleandata.push(row.reduce((map, r, i) => {
+    cleanrow = row.reduce((map, r, i) => {
       map[header[i]] = r;
       return map;
-    }, {id: index}));
+    }, {id: index});
+    cleanrow.year = +cleanrow.year;
+    cleandata.push(cleanrow);
   });
 
-  // Run any necessary data conversion
-  cleandata.forEach(function(row) {
-    row.year = +row.year;
-  });
-
-  // Create Lineage Tree
-  tree = new LineageTree(cleandata, "body");
+  // Trigger data processing function
+  this.buildTree(cleandata);
 }
 
-
-/*** Lineage Tree Drawing Class ***/
-function LineageTree(data, container) {
+/*** Build Tree from hierarchical data ***/
+LineageTree.prototype.buildTree = function(data) {
   let _ = this;
-  _.box = {width: 130, height: 56, margin: 20};
-  _.navHeight = 150;
-  _.logoHeight = 3/4;
-  _.logoOpacity = 0.33;
-  _.k = 1;
-  _.logoSrc = "logo.svg";
-  _.body = d3.select(container);
-  _.svg = _.body.append("svg").attr("width", "100%").attr("height", "100%");
-  _.nav = _.body.append("div").attr("class", "nav container").append("svg");
-  _.view = _.svg.append("g").attr("class", "view-box");
-
-  // Set up background containers
-  let bkgSource = _.body.append("div").style("display", "none")
-      .append("svg").attr("id", "background");
-  _.viewUse = _.view.append("use").attr("xlink:href", "#background")
-      .attr("opacity", _.logoOpacity);
-  _.navUse = _.nav.append("use").attr("xlink:href", "#background")
-      .attr("opacity", _.logoOpacity);
-
-  // Stratify Data
+  // Add Dummy Root, and link parentless nodes to it
   data.forEach(function(d) {
     if (data.filter(f => f.name == d.parent).length == 0) d.parent = "N/A";
   })
   data.splice(0, 0, {name: "N/A", year: d3.min(data, d => d.year) - 1,
                      parent: null, dummy: true});
-  _.root = d3.stratify().id(d => d.name).parentId(d => d.parent)(data);
+
+  // Stratify data and pull out nodes, links, actives
+  _.root = _.stratify(data);
   _.nodeList = _.root.descendants();
   _.linkList = _.root.links();
+  _.actives = _.nodeList.filter(n => n.data.active);
 
   // Save parent / child references before manipulating tree
   _.nodeList.forEach(n => {
@@ -79,24 +110,15 @@ function LineageTree(data, container) {
     while (n.data.year <= n.parent.data.year) {
       _.elevateNode(i);
     }
-    // Insert spacers if more than one year between this and parent
+    // If this node is more than one year less than its parent's year,
+    // add a spacer to move it down the tree
     if (n.data.year - n.parent.data.year > 1) {
       _.insertParent(i);
     }
   }
 
   // Tree function - Calculate node positions
-  d3.tree()
-      .nodeSize([_.box.width + _.box.margin, _.box.height + _.box.margin])
-      .separation((a, b) => {
-        // If we share a parent, keep padding tight
-        if (a.parent == b.parent) return 1;
-        // Otherwise, add padding so we don't overlap either node's children
-        let sep = 1;
-        if (a.children) sep += (a.children.length - 1) / 2;
-        if (b.children) sep += (b.children.length - 1) / 2;
-        return sep;
-      })(_.root);
+  _.tree(_.root);
 
   // Remove dummy nodes
   _.nodeList = _.nodeList.filter(n => !n.data.dummy);
@@ -105,141 +127,25 @@ function LineageTree(data, container) {
     n.children = n.realChildren;
   })
 
-  // Draw lineage tree
-  // Draw links
-  _.link = _.view.append("g").attr("class", "link-layer")
-      .selectAll(".link").data(_.linkList).enter()
-      .append("path")
-          .attr("class", d => "link t-" + d.target.data.id)
-          .attr("d", _.linkPathGen());
-
-  // Add nodes
-  _.node = _.view.append("g").attr("class", "node-layer")
-      .selectAll(".node").data(_.nodeList).enter()
-      .append("g")
-          .attr("id", d => "b-" + d.data.id)
-          .attr("class", d => "node" + (d.data.active ? " node-undergrad" : ""))
-          .attr("transform", d => "translate(" + d.x + "," + d.y + ")");
-  // Add node content
-  _.node.append("rect")
-      .attr("class", "node-box")
-      .attr("x", -_.box.width / 2)
-      .attr("width", _.box.width)
-      .attr("height", _.box.height)
-      .on("click", _.activateNode);
-  _.node.append("text")
-      .attr("dy", 16)
-      .style("text-anchor", "middle")
-      .text(d => d.data.name)
-      .on("click", _.activateNode);
-  _.node.append("text")
-      .attr("dy", 32)
-      .style("text-anchor", "middle")
-      .text(d => d.data.year_label)
-      .on("click", _.activateNode);
-  _.node.append("text")
-      .attr("dy", 48)
-      .style("text-anchor", "middle")
-      .text(d => d.data.pledge_label)
-      .on("click", _.activateNode);
-
   // Get dimensions of Tree
-  let leaves = _.root.leaves();
-  let xMin = d3.min(leaves, d => d.x) - (_.box.width + _.box.margin) / 2;
-  let xMax = d3.max(leaves, d => d.x) + (_.box.width + _.box.margin) / 2;
-  let yMin = 0 - _.box.margin / 2;
-  let yMax = d3.max(leaves, d => d.y) + _.box.height + _.box.margin / 2;
+  _.xMin = d3.min(_.nodeList, d => d.x) - (_.box.width + _.box.margin) / 2;
+  _.xMax = d3.max(_.nodeList, d => d.x) + (_.box.width + _.box.margin) / 2;
+  _.yMin = d3.min(_.nodeList, d => d.y) - _.box.margin / 2;
+  _.yMax = d3.max(_.nodeList, d => d.y) + _.box.height + _.box.margin / 2;
 
-  // Define nav box scales
-  _.navWidth = _.navHeight * (xMax - xMin) / (yMax - yMin);
-  _.navScaleY = d3.scaleLinear().domain([yMin, yMax])
-      .range([0, _.navHeight]);
-  _.navScaleX = d3.scaleLinear().domain([xMin, xMax])
-      .range([0, _.navWidth]);
+  // Set Navbox dimension / scales
+  _.navWidth = _.navHeight * (_.xMax - _.xMin) / (_.yMax - _.yMin);
+  _.navScaleY.domain([_.yMin, _.yMax]).range([0, _.navHeight]);
+  _.navScaleX.domain([_.xMin, _.xMax]).range([0, _.navWidth]);
 
-  // Set nav box size
-  _.nav.attr("height", _.navHeight).attr("width", _.navWidth);
-  // Draw nav links
-  _.nav.append("g").attr("class", "links")
-      .selectAll(".link").data(_.linkList).enter()
-          .append("path")
-              .attr("class", d => "link link-nav t-" + d.target.data.id)
-              .attr("d", _.linkPathGen(_.navScaleX, _.navScaleY));
-  // Add dots for undergrad nodes
-  _.actives = _.nodeList.filter(n => n.data.active);
-  _.nav.append("g").attr("class", "undergrads")
-      .selectAll(".dot").data(_.actives).enter()
-      .append("circle")
-          .attr("class", d => "dot a-" + d.data.id)
-          .attr("cx", d => _.navScaleX(d.x))
-          .attr("cy", d => _.navScaleY(d.y))
-          .attr("r", 1.5);
-
-  // Load Background
-  d3.xml(_.logoSrc).then(function(svg) {
-    let svgTag = svg.getElementsByTagName("svg")[0];
-    bkgSource.node().appendChild(svgTag);
-    let bkgX = +svgTag.getAttribute("width");
-    let bkgY = +svgTag.getAttribute("height");
-    let viewScale = yMax / bkgY * _.logoHeight;
-    _.viewUse.attr("transform", "translate(" + -bkgX * viewScale / 2 +
-                   "," + (_.box.height + _.box.margin) * 2 + ") " +
-                   "scale(" + viewScale  + ")");
-    let navScale = _.navHeight / bkgY * _.logoHeight;
-    _.navUse.attr("width", bkgX).attr("height", bkgY)
-      .attr("transform", "translate(" + (_.navScaleX(0) - bkgX * navScale / 2) +
-            "," + _.navScaleY(_.box.height + _.box.margin) * 2 + ") " +
-            "scale(" + navScale + ")");
-  });
-
-  // Initialize Brush
-  _.brush = _.nav.append("rect").attr("class", "brush");
-  _.updateBrush({x: 0, y: 0});
-
-  // Initialize Pan & Zoom behavior
-  _.zoom = d3.zoom()
-      .scaleExtent([0.5, 1])
-      .translateExtent([[xMin, yMin], [xMax, yMax]])
-      .on("zoom", zoomed);
-  function zoomed() {
-    _.k = d3.event.transform.k;
-    _.view.attr("transform", d3.event.transform);
-    _.updateBrush(d3.event.transform);
-  }
-  _.svg.call(_.zoom);
-
-  // Initialize Drag behavior
-  _.dragOffset = {x: 0, y: 0};
-  _.drag = d3.drag()
-      .on("start", dragStart)
-      .on("drag", dragged);
-  function dragStart() {
-    _.dragOffset.x = d3.event.x - d3.select(this).attr("x");
-    _.dragOffset.y = d3.event.y - d3.select(this).attr("y");
-  }
-  function dragged() {
-    let x = d3.event.x - _.dragOffset.x;
-    let y = d3.event.y - _.dragOffset.y;
-    let b = d3.select(this);
-    x = Math.max(0, Math.min(x, _.navWidth - b.attr("width")));
-    y = Math.max(0, Math.min(y, _.navHeight - b.attr("height")));
-    _.svg.call(_.zoom.translateTo,
-               _.navScaleX.invert(x + b.attr("width") / 2),
-               _.navScaleY.invert(y + b.attr("height") / 2)
-               );
-    b.attr("x", x).attr("y", y);
-  }
-  _.brush.call(_.drag);
+  // Trigger Graphing functions
+  _.loadBackground();
+  _.drawGraphAndNav();
 
   // Initialize additional features
   _.initSearch();
   _.initInfoBox();
 
-  // Add Resize Listener
-  window.addEventListener("resize", () => _.updateBrush());
-
-  // Move to Center
-  _.svg.call(_.zoom.translateTo, 0, 0);
 }
 
 /*** LineageTree Tree Manipulation Functions ***/
@@ -291,6 +197,128 @@ LineageTree.prototype.elevateNode = function(i) {
     spacer.depth = p.depth;
     p.parent.children.splice(0, 0, spacer);
     this.nodeList.splice(i, 0, spacer);
+  }
+}
+
+/*** LineageTree Graphing functions ***/
+LineageTree.prototype.drawGraphAndNav = function() {
+  let _ = this;
+  // Draw links
+  _.view.select(".link-layer").selectAll(".link").data(_.linkList).enter()
+      .append("path")
+          .attr("class", d => "link t-" + d.target.data.id)
+          .attr("d", _.linkPathGen());
+  // Add nodes
+  _.node = _.view.select(".node-layer")
+      .selectAll(".node").data(_.nodeList).enter()
+      .append("g")
+          .attr("id", d => "b-" + d.data.id)
+          .attr("class", d => "node" + (d.data.active ? " node-undergrad" : ""))
+          .attr("transform", d => "translate(" + d.x + "," + d.y + ")");
+  // Add node contents
+  _.node.append("rect")
+      .attr("class", "node-box")
+      .attr("x", -_.box.width / 2)
+      .attr("width", _.box.width)
+      .attr("height", _.box.height)
+      .on("click", _.activateNode);
+  _.node.append("text")
+      .attr("dy", 16)
+      .style("text-anchor", "middle")
+      .text(d => d.data.name)
+      .on("click", _.activateNode);
+  _.node.append("text")
+      .attr("dy", 32)
+      .style("text-anchor", "middle")
+      .text(d => d.data.year_label)
+      .on("click", _.activateNode);
+  _.node.append("text")
+      .attr("dy", 48)
+      .style("text-anchor", "middle")
+      .text(d => d.data.pledge_label)
+      .on("click", _.activateNode);
+
+  // Set nav box size
+  _.nav.attr("height", _.navHeight).attr("width", _.navWidth);
+
+  // Draw nav links
+  _.nav.select(".link-layer").selectAll(".link").data(_.linkList).enter()
+      .append("path")
+          .attr("class", d => "link link-nav t-" + d.target.data.id)
+          .attr("d", _.linkPathGen(_.navScaleX, _.navScaleY));
+  // Add dots for undergrad nodes
+  _.nav.select(".node-layer").selectAll(".dot").data(_.actives).enter()
+      .append("circle")
+          .attr("class", d => "dot a-" + d.data.id)
+          .attr("cx", d => _.navScaleX(d.x))
+          .attr("cy", d => _.navScaleY(d.y))
+          .attr("r", 1.5);
+
+  // Initialize Brush
+  _.brush = _.nav.append("rect").attr("class", "brush");
+  _.updateBrush({x: 0, y: 0});
+
+  // Initialize Pan & Zoom behavior
+  _.zoom = d3.zoom()
+      .scaleExtent([0.5, 1])
+      .translateExtent([[_.xMin, _.yMin], [_.xMax, _.yMax]])
+      .on("zoom", zoomed);
+  function zoomed() {
+    _.k = d3.event.transform.k;
+    _.view.attr("transform", d3.event.transform);
+    _.updateBrush(d3.event.transform);
+  }
+  _.svg.call(_.zoom);
+
+  // Initialize Drag behavior
+  _.dragOffset = {x: 0, y: 0};
+  _.drag = d3.drag()
+      .on("start", dragStart)
+      .on("drag", dragged);
+  function dragStart() {
+    _.dragOffset.x = d3.event.x - d3.select(this).attr("x");
+    _.dragOffset.y = d3.event.y - d3.select(this).attr("y");
+  }
+  function dragged() {
+    let x = d3.event.x - _.dragOffset.x;
+    let y = d3.event.y - _.dragOffset.y;
+    let b = d3.select(this);
+    x = Math.max(0, Math.min(x, _.navWidth - b.attr("width")));
+    y = Math.max(0, Math.min(y, _.navHeight - b.attr("height")));
+    _.svg.call(_.zoom.translateTo,
+               _.navScaleX.invert(x + b.attr("width") / 2),
+               _.navScaleY.invert(y + b.attr("height") / 2)
+               );
+    b.attr("x", x).attr("y", y);
+  }
+  _.brush.call(_.drag);
+
+  // Add Resize Listener
+  window.addEventListener("resize", () => _.updateBrush());
+
+  // Move to Center
+  _.svg.call(_.zoom.translateTo, 0, 0);
+}
+
+LineageTree.prototype.loadBackground = function() {
+  let _ = this;
+  if (_.backgroundURL) {
+    // Load Background
+    d3.xml(_.backgroundURL).then(function(svg) {
+      let svgTag = svg.getElementsByTagName("svg")[0];
+      _.bkgSource.node().appendChild(svgTag);
+      let bkgX = +svgTag.getAttribute("width");
+      let bkgY = +svgTag.getAttribute("height");
+      let viewScale = _.yMax / bkgY * _.logoHeight;
+      let dx = -bkgX / 2;
+      let dy = _.box.height + _.box.margin * 2;
+      _.viewUse.attr("transform", "translate(" + dx * viewScale + "," +
+                     dy + ") scale(" + viewScale  + ")");
+      let navScale = _.navHeight / bkgY * _.logoHeight;
+      _.navUse.attr("width", bkgX).attr("height", bkgY)
+        .attr("transform", "translate(" + (_.navScaleX(0) + dx * navScale) +
+              "," + _.navScaleY(dy) * 2 + ") scale(" + navScale + ")");
+    });
   }
 }
 
